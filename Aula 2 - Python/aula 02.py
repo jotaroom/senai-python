35c865aaf8dec8e2c7466f1c0bebc47a096ed882
João F. Bernardes <114032595+jotaroom@users.noreply.github.com>
2024-12-03 09:55:15 -0300

    Atualizações da aula 2 do curso de python.

---
+++ Aula 2 - Python/aula 02.py
@@ -0,0 +1,11 @@
+print('Bom dia a todos')
+
+nome_usuario = 'João Felipe' #string
+idade_usuario = 17 #int
+altura_usuario = 1.93 #tipo float = ponto flutuante
+maior_idade = False
+
+print('Seja bem-vindo', nome_usuario)
+print('Sua idade é', idade_usuario, 'anos')
+print('Você tem', altura_usuario, 'metros')
+print('Você é maior de idade?', maior_idade)
